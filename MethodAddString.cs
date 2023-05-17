0adb39e7b441aa1563eb0914172b34d6d022ba83
Буторов Артем Михайлович <gr604_buami@ttit.local>
2023-05-17 17:13:24 +0700

    Added MethodAddString.cs

---
+++ MethodAddString.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace CalculatorPractice.MethodsFunctions
+{
+    public class MethodAddString
+    {
+        public static void AddStr(string str)
+        {
+            File.AppendAllText(@"C:\Users\gr604_buami\source\repos\CalculatorPractice\CalculatorPractice\Data.txt", str);
+        }
+    }
+}
