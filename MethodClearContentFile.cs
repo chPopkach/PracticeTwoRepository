690a1720961f884b199540cbeaa3af8c566dfa86
Буторов Артем Михайлович <gr604_buami@ttit.local>
2023-05-17 17:16:12 +0700

    Added MethodClearContentFile.cs

---
+++ MethodClearContentFile.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace CalculatorPractice.MethodsFunctions
+{
+    public class MethodClearContentFile
+    {
+        public static void ClearContent()
+        {
+            File.WriteAllText(@"C:\Users\gr604_buami\source\repos\CalculatorPractice\CalculatorPractice\Data.txt", string.Empty);
+        }
+    }
+}
