19a5cd1ff20d01860437151e24811484abedbf1b
Буторов Артем Михайлович <gr604_buami@ttit.local>
2023-05-17 17:17:41 +0700

    Added MethodShowFile.cs

---
+++ MethodShowFile.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace CalculatorPractice.MethodsFunctions
+{
+    public class MethodShowFile
+    {
+        public static void Show()
+        {
+            Process.Start(new ProcessStartInfo { FileName = @"C:\Users\gr604_buami\source\repos\CalculatorPractice\CalculatorPractice\Data.txt", UseShellExecute = true });
+        }
+    }
+}
