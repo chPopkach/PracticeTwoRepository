b35d84b9291bf029bba0bdb343cc657a1b8072d8
Буторов Артем Михайлович <gr604_buami@ttit.local>
2023-05-17 17:10:09 +0700

    Added void project

---
+++ CalculatorPractice/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CalculatorPractice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CalculatorPractice/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CalculatorPractice.MethodsFunctions;
+using System.Windows;
+
+namespace CalculatorPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearContentFile(object sender, RoutedEventArgs e)
+        {
+            MethodClearContentFile.ClearContent();
+        }
+
+        private void ShowFile(object sender, RoutedEventArgs e)
+        {
+            MethodShowFile.Show();
+        }
+
+        private void AddString(object sender, RoutedEventArgs e)
+        {
+            MethodAddString.AddStr(textBox_string.Text);
+        }
+
+        private void ClearString(object sender, RoutedEventArgs e)
+        {
+            textBox_string.Text = MethodClearString.ClearStr();
+        }
+    }
+}
